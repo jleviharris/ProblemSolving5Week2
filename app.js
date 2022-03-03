dc0b6277e3c751244136055514410c03741bf5a9
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-03-03 14:16:01 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict"
+// 1. Create a method to print duplicate characters from a string
+
+// 2. Create a method that reverses a string using recursion. You may not use any built-in class library methods
+
+// 3.  Create a method that counts the number of vowels and consonants in a given string
+
+// 4. Create a method that finds all the permutations of a given string.
+
+// 5. Create a method that takes in two rectangle objects and checks if two rectangle objects overlap each other. 
+
+// 6. Implement a quicksort algorithm, insertion sort algorithm, or bucket sort algorithm (all three for the extra challenge)
